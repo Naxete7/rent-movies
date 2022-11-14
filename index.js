c24e8ab79773178664dca0f83ecb44502046a3fc
Ignacio <naxete7@hotmail.es>
2022-11-14 20:01:33 +0100

    morgan funcionando

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const db = require('./db.js');
 const router = require('./router.js');
 
 const app = express();
-const PORT = process.env.PORT || 3005; //Configuramos puerto heroku
+const PORT = process.env.PORT || 3005; 
 
 //Middleware
-//app.use(morgan('combined', { stream: logger.stream }));
+app.use(morgan('combined', { stream: logger.stream }));
 app.use(express.json());
 
 //Rutas
